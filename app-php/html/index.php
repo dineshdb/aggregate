5c7efd4f7ba25584023a371bd4e31fb37f17617e
Dinesh Bhattarai <dineshdb@dbhattarai.info.np>
2019-06-16 17:01:38 +0545

    Add request type checking

--- app-php/html/index.php
+++ app-php/html/index.php
@@ -11,5 +11,11 @@ $conn = new pdo("mysql:host=$servername;dbname=db", $username, $password);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+   // Do api works here
+} else {
+	// render the html ui here
+}
 ?> 
+
+
