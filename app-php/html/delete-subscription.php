cf04eeac6f36b83bbcdd079c3d4b2a6f52e9801d
Aashutosh Poudel <aashutsohpoudyal@gmail.com>
2019-07-11 22:44:10 +0545

    added edit, delete, manage subs

---
+++ app-php/html/delete-subscription.php
@@ -0,0 +1,32 @@
+<?php
+
+session_start();
+
+// Check if the user is logged in
+if(isset($_SESSION["loggedin"]) == false || $_SESSION["loggedin"] === false){
+    header("location: login.php");
+    exit;
+}
+
+require_once "config.php";
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    $pageId = $_POST["pageId"];
+
+    $pgs_sql = "DELETE FROM pages 
+            WHERE pageId = ?";
+    $subs_sql = "DELETE FROM subscriptions
+                WHERE pageId = ?";
+
+    $stmt = $link->prepare($pgs_sql);
+    $stmt->execute(array($pageId));
+
+    $stmt = $link->prepare($subs_sql);
+    $stmt->execute(array($pageId));
+    
+    header("location: manage-subscription.php");
+    exit();
+}
+
+?>
---
+++ app-php/html/edit-subscription.php
@@ -0,0 +1,67 @@
+<?php
+
+// Initialize the session
+session_start();
+
+// Check if the user is already logged in
+if(isset($_SESSION["loggedin"]) == false || $_SESSION["loggedin"] === false){
+    header("location: login.php");
+    exit;
+}
+
+// Include config file
+require_once "config.php";
+
+$url = $title = "";
+$url_err = $title_err = "";
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    $pageId = $_POST["pageId"];
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Add Subscriptions</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+
+<body>
+    <div class="wrapper">
+        
+        <h2>Add Subscriptions</h2>
+        <p>Add site URL and a title</p>
+
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        
+            <div class="form-group <?php echo (!empty($url_err)) ? 'has-error' : ''; ?>">
+                <label>URL</label>
+                <input type="text" name="url" class="form-control" value="<?php echo $url; ?>">
+                <span class="help-block"><?php echo $url_err; ?></span>
+            </div>    
+
+            <div class="form-group <?php echo (!empty($title_err)) ? 'has-error' : ''; ?>">
+                <label>Title</label>
+                <input type="text" name="title" class="form-control"  value="<?php echo $title; ?>">
+                <span class="help-block"><?php echo $title_err; ?></span>
+            </div>
+
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Update">
+            </div>
+
+            <p><a href="manage-subscription.php">Back</a>.</p>
+        </form>
+    </div>  
+
+</body>
+</html>
